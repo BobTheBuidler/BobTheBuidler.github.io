b49f5dffe09e96027b832a040d2549d93001f3ab
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2021-07-12 14:29:20 -0400

    chore: add site-wide NFA disclaimer

--- NavBar/ddmenu.js
+++ NavBar/ddmenu.js
@@ -1,3 +1,14 @@
+/* This is the code for the Disclaimer popup, not the menu */
+$(document).ready(function(){
+  let data = sessionStorage.getItem('disclaimerShown');
+  if(!data) {
+    alert('TEST ALERT');
+  }
+    sessionStorage.setItem('disclaimerShown','true')
+  });
+/* End Disclaimer popup code */
+/* Start nav menu code */ 
+
 var ddmenuOptions = {
     menuId: "ddmenu",
     linkIdToMenuHtml: "ddmenuLink",
